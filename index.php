c9f69dafa11e575c03426dd4af7213d69e67ced1
officer <officer@enterprise.devices.christran.in>
2015-01-17 05:44:44 -0500

    login

--- index.php
+++ index.php
@@ -4,4 +4,11 @@
 		<title>Strive</title>
 		<link href="assets/css/app.css" type="text/css" />
 	</head>
-	
+	<body>
+		<div class="login-container">
+			<input type="text" name="email" id="email" required>
+			<input type="password" name="password" id="password" required>
+			<input type="submit" class="submit" value="Submit" />
+		</div>
+	</body>
+</html>
