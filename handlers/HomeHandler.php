abf2c4233c6ce04c6026c0770a731b332da4fc76
Michael Meyer <michaelmeyer@0587359100.wireless.umich.net>
2015-01-17 13:07:08 -0500

    Damn semicolons

--- handlers/HomeHandler.php
+++ handlers/HomeHandler.php
@@ -2,7 +2,7 @@
 class HomeHandler
 {
   public function get() {
-    $page = "Home"
+    $page = "Home";
     include("../pages/elements/header.tpl.html");
     include("../pages/index.tpl.html");
     include("../pages/elements/footer.tpl.html");
