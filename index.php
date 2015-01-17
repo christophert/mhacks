7433c29ee44b971b10b8d1889509da0b6a4b5dd3
officer <officer@enterprise.devices.christran.in>
2015-01-17 07:34:20 -0500

    update less

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Strive</title>
-		<link href="assets/css/app.css" type="text/css" rel="stylesheet" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Strive | Login</title>
+
+	<!-- Bootstrap -->
+	<link href="assets/css/app.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 	</head>
 	<body>
-		<div class="login-container">
+  		<div class="login-container">
 			<form class="signin">
 				<input type="text" name="email" id="email" required>
 				<input type="password" name="password" id="password" required>
 				<input type="submit" class="submit" value="Submit" />
 			</form>
 		</div>
+		<div id="footer">		
+		<div class="container">
+			<small id="commit">Commit <?php echo substr(`git rev-parse --verify HEAD`,0,7); ?></small>
+		</div>
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	</body>
 </html>
