7489852f11771ff813e58ec8d2e42cdeb13af40d
Michael Meyer <michaelmeyer@0587352446.wireless.umich.net>
2015-01-17 21:23:06 -0500

    pass db parameter

--- handlers/EntriesHandler.php
+++ handlers/EntriesHandler.php
@@ -14,7 +14,7 @@ class EntriesHandler
     include("../pages/elements/footer.tpl.html");
   }
 
-  private function getEntries() {
+  private function getEntries($db) {
     $query = $db->prepare("SELECT entries.hrs, entries.confirm, events.name FROM `entries` INNER JOIN `events` on entries.event = events.id WHERE `uid`=:uid");
     $query->bindParam(':uid', $_SESSION['userId'], PDO::PARAM_STR);
     $query->execute();
