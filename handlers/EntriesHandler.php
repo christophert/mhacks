ac9815ac45fec658450e15d536c785522b713398
Michael Meyer <michaelmeyer@0587352446.wireless.umich.net>
2015-01-17 21:31:27 -0500

    add debug for user id session var

--- handlers/EntriesHandler.php
+++ handlers/EntriesHandler.php
@@ -16,6 +16,7 @@ class EntriesHandler
 
   private function getEntries($db) {
     $query = $db->prepare("SELECT entries.hrs, entries.confirm, events.name FROM `entries` INNER JOIN `events` on entries.event = events.id WHERE `uid`=:uid");
+    echo $_SESSION['userId'];
     $query->bindParam(':uid', $_SESSION['userId'], PDO::PARAM_STR);
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
