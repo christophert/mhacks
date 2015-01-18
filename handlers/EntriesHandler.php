11cf77f7a9a8fd0da7473b4563e3afdc4a304cb2
Michael Meyer <michaelmeyer@0587352446.wireless.umich.net>
2015-01-17 21:08:57 -0500

    Entries page

---
+++ handlers/EntriesHandler.php
@@ -0,0 +1,23 @@
+<?php
+class EntriesHandler
+{
+  public function get() {
+    $page = "Entries";
+
+    $dbObj = new DatabaseConnection();
+    $db = $dbObj->connect();
+
+    $entries = $this->getEvents($db);
+
+    include("../pages/elements/header.tpl.html");
+    include("../pages/entries.tpl.html");
+    include("../pages/elements/footer.tpl.html");
+  }
+
+  private function getEntries() {
+    $query = $db->prepare("SELECT entries.hrs, entries.confirm, events.name FROM `entries` INNER JOIN `events` on entries.event = events.id WHERE `uid`=:uid");
+    $query->bindParam(':uid', $_SESSION['userId'], PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
