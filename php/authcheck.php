d54042e013c9d28d59ef81b0f6d7ad0fe7f0aed1
officer <officer@enterprise.devices.christran.in>
2015-01-18 09:12:51 -0500

    authcheck

--- php/authcheck.php
+++ php/authcheck.php
@@ -4,7 +4,7 @@
 	//Check whether the session variable SESS_ID is present or not
 	if(!strpos($_SERVER['REQUEST_URI'],"login")) {
 		session_start();
-		if(!isset($_SESSION['userId']) || (trim($_SESSION['userId']) == '')) 
+		if(!isset($_SESSION['userId'])) 
 		{
 			$errMsg_arr[] = 'Access Denied - Please login';
 			$_SESSION['ERRMSG_ARR'] = $errMsg_arr;
