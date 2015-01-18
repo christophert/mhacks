484b116442d9eda0052ddc0f6a8a0cb0135b6089
officer <officer@enterprise.devices.christran.in>
2015-01-17 22:11:02 -0500

    maybe a chance, data

--- handlers/LoginHandler.php
+++ handlers/LoginHandler.php
@@ -14,10 +14,10 @@ class LoginHandler
     $workable->setId($_POST['email']);
     $response = $workable->precheckUser($_POST['password']);
     if($response['status'] == "OK") {
+    	$response = $response['data'];
     	$_SESSION['userId'] = $response['userId'];
     	$_SESSION['name'] = $response['name'];
     	$_SESSION['email'] = $response['email'];
-    	var_dump($response);
     	header('Location: /?success');
     }
     else
