ce61d942a3938fd54875d58a595b7c655edbef15
officer <officer@enterprise.devices.christran.in>
2015-01-17 23:51:03 -0500

    daf

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ require($_SERVER['DOCUMENT_ROOT']."/../../dbcreds.inc");
 //####################################################################
 //die("Strive is currently undergoing maintenance and will be back up soon.");
 date_default_timezone_set('America/New_York');
-require($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
+//require($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
 require('../vendor/autoload.php');
 
 require('../php/database.inc');
@@ -17,6 +17,7 @@ require('../handlers/EventsHandler.php');
 require('../handlers/EventsDetailHandler.php');
 require('../handlers/EntriesHandler.php');
 require('../handlers/LogoutHandler.php');
+
 Toro::serve(array(
   '/'               => 'HomeHandler',
   '/login'          => 'LoginHandler',
