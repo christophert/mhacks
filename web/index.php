2d6a10c6e8618f64a48409c59ccd1e6f9d9a93f9
officer <officer@enterprise.devices.christran.in>
2015-01-18 09:09:00 -0500

    authcheck

--- web/index.php
+++ web/index.php
@@ -1,13 +1,14 @@
 <?php
 session_save_path("/tmp/session");
-session_start();
+//session_start();
+require($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
 require($_SERVER['DOCUMENT_ROOT']."/../../dbcreds.inc");
 //####################################################################
 // UNCOMMENT BELOW LINE TO PUT STRIVE INTO MAINTENANCE MODE
 //####################################################################
 //die("Strive is currently undergoing maintenance and will be back up soon.");
 date_default_timezone_set('America/New_York');
-//require($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
+require($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
 require('../vendor/autoload.php');
 
 require('../php/database.inc');
