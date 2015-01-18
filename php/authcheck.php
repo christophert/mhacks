6fcdf4863c299710717ead336bb289fb75b887ac
officer <officer@enterprise.devices.christran.in>
2015-01-17 22:30:22 -0500

    workpls

---
+++ php/authcheck.php
@@ -0,0 +1,13 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Check whether the session variable SESS_ID is present or not
+	if(!isset($_SESSION['userId']) || (trim($_SESSION['userId']) == '')) 
+	{
+		$errMsg_arr[] = 'Access Denied - Please login';
+		$_SESSION['ERRMSG_ARR'] = $errMsg_arr;
+		header("location: admin_error.php");
+		exit();
+	}
+?>
