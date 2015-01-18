e6f1cd7d19967f2ff917b304c3ed2d10332d3e1e
officer <officer@enterprise.devices.christran.in>
2015-01-18 10:29:37 -0500

    privs

--- handlers/LogoutHandler.php
+++ handlers/LogoutHandler.php
@@ -1,9 +1,9 @@
 <?php
 class LogoutHandler {
 	public function get() {
-		include($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
+		//include($_SERVER['DOCUMENT_ROOT']."/../php/authcheck.php");
 		
-		session_start();
+		//session_start();
 		unset($_SESSION['userId']);
 		unset($_SESSION['name']);
 		unset($_SESSION['email']);
