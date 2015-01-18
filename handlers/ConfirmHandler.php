5ac5dac472231caa7ad3099ca897cd64a9e2f11c
officer <officer@enterprise.devices.christran.in>
2015-01-18 02:27:21 -0500

    semicolon bitch

--- handlers/ConfirmHandler.php
+++ handlers/ConfirmHandler.php
@@ -1,13 +1,14 @@
 <?php
 class ConfirmHandler {
 	public function get() {
-		$page = "Confirm Attendees"
+		$page = "Confirm Attendees";
 		include("../pages/header.tpl.html");
 		include("../pages/confirm.tpl.html");
 		include("../pages/footer.tpl.html");
 	}
 
 	public function post() {
-		die();
+		include("../php/database.inc");
+		$workable = new DatabaseConnection();
 	}
 }
