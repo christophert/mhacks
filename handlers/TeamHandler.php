25aa1983b0edae8bbc62e4c666b37b0700400c5b
officer <officer@enterprise.devices.christran.in>
2015-01-18 11:47:21 -0500

    js

--- handlers/TeamHandler.php
+++ handlers/TeamHandler.php
@@ -34,21 +34,21 @@ class TeamHandler {
 	private function getNameCompanies($db) {
 		$query = $db->prepare("SELECT name FROM teams");
 		$query->execute();
-		$result = $query->fetch(PDO::FETCH_ASSOC);
+		$result = $query->fetchAll();
 		return $result;
 	}
 
 	private function getHourCompanies($db) {
 		$query = $db->prepare("SELECT hrs FROM teams");
 		$query->execute();
-		$result = $query->fetch(PDO::FETCH_ASSOC);;
+		$result = $query->fetchAll();
 		return $result;
 	}
 
 	private function getAvgHourCompanies($db) {
 		$query = $db->prepare("SELECT avghrs FROM teams");
 		$query->execute();
-		$result = $query->fetch(PDO::FETCH_ASSOC);;
+		$result = $query->fetchAll();
 		return $result;
 	}
 }
