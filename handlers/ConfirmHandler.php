a053d6ed6aae1f1293ff6e8a7002c7af6938e1da
officer <officer@enterprise.devices.christran.in>
2015-01-18 03:21:08 -0500

    the span

--- handlers/ConfirmHandler.php
+++ handlers/ConfirmHandler.php
@@ -2,13 +2,22 @@
 class ConfirmHandler {
 	public function get() {
 		$page = "Confirm Attendees";
+		$workable = new DatabaseConnection();
+		$db = $workable->connect();
+		$query = $db->prepare("SELECT u.name FROM users AS u INNER JOIN rsvp AS rs on u.id=rs.uid");
+		$query->execute();
+		$result = $query->fetch(PDO::FETCH_ASSOC);
 		include("../pages/header.tpl.html");
 		include("../pages/confirm.tpl.html");
 		include("../pages/footer.tpl.html");
 	}
 
 	public function post() {
-		include("../php/database.inc");
-		$workable = new DatabaseConnection();
+		if($_POST['action'] == "update") {	
+			include("../php/database.inc");
+			$workable = new DatabaseConnection();
+			$db = $workable->connect();
+			$query = $db->prepare("")
+		}
 	}
 }
